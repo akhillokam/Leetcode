ad168fa3d7b7923992fc416130c24bcd85ac304d
akhillokam <shammi.akhil97@gmail.com>
2022-10-07 18:55:30 -0500

    LC-1209

---
+++ Stack/RemoveadjdupsinStr.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()[0]==c){
+                stack.peek()[1]++;
+            } else {
+                stack.push(new int[]{c,1});
+            }
+
+            if(stack.peek()[1]==k){
+                stack.pop();
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            int []top = stack.pop();
+
+            while(top[1]-->0){
+                sb.append((char) top[0]);
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
