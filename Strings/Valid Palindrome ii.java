4e11ab6f930153862035e0c8bf1d78010cb0ed76
akhillokam <shammi.akhil97@gmail.com>
2022-10-08 12:25:44 -0500

    LC-680

---
+++ Strings/Valid Palindrome ii.java
@@ -0,0 +1,29 @@
+//O(n)
+//O(1)
+class Solution {
+    public boolean helper(String s, int i, int j){
+        
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return (helper(s,i+1,j) || helper(s,i,j-1));
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
